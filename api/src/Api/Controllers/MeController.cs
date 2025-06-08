b06a61ae97eeab2ebe49cb27c8e418d8e6574aec
leo-oliveira-eng <leo.oliveira.eng@outlook.com>
2025-06-07 22:49:28 -0300

    feat: add the basic structure of the project

---
+++ api/src/Api/Controllers/MeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController, Route("api/[controller]")]
+public class MeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok(new { name = "CDB Yield Simulator", version = "1.0.0" });
+}
